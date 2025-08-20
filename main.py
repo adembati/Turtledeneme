d67a7152efe3cb138bbff6d10eadf38ef7d1d03b
Adem BATI <adembati@gmail.com>
2025-08-20 12:57:19 +0300

    square added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import turtle
 drawing_board=turtle.Screen()
-drawing_board.bgcolor("green")
+drawing_board.bgcolor("lightgreen")
+drawing_board.setup(width=600, height=600)
 drawing_board.title("Python Turtle")
 turtle_instance=turtle.Turtle()
-turtle_instance.forward(100)
-turtle_instance.left(90)
-turtle_instance.forward(100)
+for i in range(4):
+    turtle_instance.forward(100)
+    turtle_instance.left(90)
+
 turtle.done()
 
